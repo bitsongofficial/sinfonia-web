b70993960fa36cc454ae7297566310e1a018fd2b
DavideSegullo <davidesegullo@gmail.com>
2022-06-23 17:09:23 +0200

    feat: :lipstick: add base typography setup for headers and paragraph

--- app.vue
+++ app.vue
@@ -3,12 +3,16 @@
 <template>
 	<Body class="bg-primary"></Body>
 	<div class="container mx-auto px-7.5">
-		<div class="grid grid-cols-12 gap-2.5">
-			<Header class="col-span-12 mt-16 md:mb-26"></Header>
+		<div class="grid grid-cols-12 gap-x-2.5">
+			<Header class="col-span-12 mt-16 mb-28 md:mb-33 lg:mb-36"></Header>
 
-			<h1 class="col-span-12 font-bold text-center text-radial-gradient-white mb-6">
+			<h1 class="col-span-12 font-bold text-center text-radial-gradient-white mb-6 md:mb-8 xl:mb-11.5">
 				Sinfonia tune<br />into Richer<br class="md:hidden" />Fan Exp
 			</h1>
+
+			<p class="col-span-12 font-medium text-white/30 text-center mb-14 md:mb-8 lg:mb-16 xl:mb-32 2xl:mb-20">
+				Fan Tokens are the ultimate way to support the music and the acts you love AND get rewarded for it.
+			</p>
 		</div>
 	</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
 		extend: {
 			spacing: {
 				7.5: '1.875rem', // 30px
+				11.5: '2.875rem', // 46px
 				15: '3.75rem', // 60px
 				26: '6.75rem', // 108px
 				33: '8.375rem', // 134px
@@ -33,17 +34,23 @@ module.exports = {
 		},
 		fontSize: {
 			xs: '.75rem',
-			sm: '.875rem',
-			tiny: '.875rem',
+			sm: '.875rem', // 14px
+			tiny: '.875rem', // 14px
 			base: '1rem', // 16px
-			lg: '1.125rem', //
-			xl: '1.5rem', // 24px
+			lg: '1.125rem', // 18px
+			p1: '1.25rem', // 20px
+			basexl: '1.5rem', // 24px
+			'1xl': '1.75rem', // 28px
+			'1.5xl': '1.875rem', // 30px
 			'2xl': '2rem', // 32px
 			'3xl': '2.25rem', // 36px
 			'4xl': ['2.5rem', '2.75rem'], // 40px
 			'5xl': '3rem', // 48px
 			'6xl': '3.25rem', // 52px
 			'7xl': ['4rem', '4.375rem'], // 64px
+			'8xl': ['5.125rem', '5.625rem'], // 82px
+			'9xl': ['6rem', '6.25rem'], // 96px
+			'10xl': ['8rem', '8rem'], // 128px
 		},
 	},
 	plugins: [],
