ee91118f0bcb07eed78dcb712afb4c99c34bbfbc
DavideSegullo <davidesegullo@gmail.com>
2022-07-09 23:40:18 +0200

    fix: :speech_balloon: fix format text

--- components/DefiSection.vue
+++ components/DefiSection.vue
@@ -2,11 +2,12 @@
 	<Grid tag="section">
 		<h5 class="col-span-12 text-white/20 font-bold mb-3 lg:mb-4 xl:mb-8 2xl:mb-9 text-center">Blockchain and DeFi</h5>
 		<h2 class="col-span-12 lg:col-span-10 lg:col-start-2 text-white text-center font-bold mb-6 lg:mb-8 2xl:mb-16">
-			A full set of tools to Empower the Music<br /><br />
+			A full set of tools to Empower the Music
 		</h2>
 
 		<p class="col-span-12 md:!text-base xl:!text-basexl 2xl:text-1.5xl text-white/30 text-center">
-			FanTokens for a new way of supporting the art of Music. Be involved and earn together with your favorite creators.
+			FanTokens for a new way of supporting the art of Music.<br />Be involved and earn together with your favorite
+			creators.
 		</p>
 	</Grid>
 </template>
