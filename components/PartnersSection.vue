510b9970ff5f84d016c2df4aa522eec0fbf7c02e
DavideSegullo <davidesegullo@gmail.com>
2022-07-09 23:48:51 +0200

    fix: :speech_balloon: fix test for partners section

--- components/PartnersSection.vue
+++ components/PartnersSection.vue
@@ -39,7 +39,7 @@ const breakpoints: {
 	<Grid tag="section">
 		<h5 class="col-span-12 text-white/20 font-bold mb-3 lg:mb-4 xl:mb-8 2xl:mb-9 text-center">Blockchain and DeFi</h5>
 		<h2 class="col-span-12 lg:col-span-10 lg:col-start-2 text-white text-center font-bold mb-16 lg:mb-20 2xl:mb-30">
-			Partner
+			Official Partner
 		</h2>
 
 		<div
