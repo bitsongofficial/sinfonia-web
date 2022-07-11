942e7738d0e1f23d0f54780dd365061a31275d3d
DavideSegullo <davidesegullo@gmail.com>
2022-07-11 22:24:27 +0200

    feat: :wrench: update countdown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -125,7 +125,7 @@ export default defineNuxtConfig({
 	modules: ['@pinia/nuxt'],
 	runtimeConfig: {
 		public: {
-			countdownDate: '2022-07-11T20:30:00.000Z',
+			countdownDate: '2022-07-11T21:00:00.000Z',
 			newsletter: {
 				apiUrl: 'https://mc.bitsong.io/',
 				listID: 'd55c3fe099',
