5af515b928b351178344376c1fd0fe04bc36231f
angelorc <angelo@rcstart.it>
2023-01-27 11:08:59 +0100

    fix: logo text

--- components/LogoText.vue
+++ components/LogoText.vue
@@ -1,9 +1,3 @@
 <template>
-	<img
-		src="@/assets/img/sinfonia-logo-text.svg?url"
-		alt="Sinfonia Text"
-		title="Sinfonia Text"
-		width="160px"
-		height="40px"
-	/>
+	<img src="@/assets/img/sinfonia-logo-text.svg?url" alt="Sinfonia Text" title="Sinfonia Text" width="160" height="40" />
 </template>
